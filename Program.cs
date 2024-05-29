5bfae249d1d10030f1d87edd5e9fd8dd024fac40
Vladimir Todarenko <todarenkov@mail.ru>
2024-05-29 19:18:39 +0600

    corrected dotnet.yml

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
-﻿public class Program {
+﻿public class Program 
+{
 
     private int A;
 
-    public Program(int A) 
+    public Program(int A)
     {
         this.A = A;
     }
 
-    public void print() 
+    public void print()
     {
         Console.WriteLine(A);
     }
 
-    static void Main(String[] args) 
+    static void Main(String[] args)
     {
 
     }
